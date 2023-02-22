ab9b0e5e22db3c22c3a71fc48719805bcc5f94a5
Michał Krokosz <michalino221@interia.pl>
2023-02-22 18:16:13 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -4,8 +4,34 @@ document.getElementById('test-button').addEventListener('click', function(){
     console.log('links:', links);
   });
   */
-  const titleClickHandler = function(){
+  const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
+  
+  /* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+  }
+
+/* add class 'active' to the clicked link */
+console.log('clickedElement:', clickedElement);
+clickedElement.classList.add ('active');
+/* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.post.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+  }
+
+/* get 'href' attribute from the clicked link */
+  const href=clickedElement.getAttribute ('href');
+
+/* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle=document.querySelector (href);
+/* add class 'active' to the correct article */
+ targetArticle.classList.add ('active');
   }
   
   const links = document.querySelectorAll('.titles a');

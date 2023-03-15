f836ccbbfbd0f2232b72a3ffd60662460bf56a89
Michał Krokosz <michalino221@interia.pl>
2023-03-15 18:18:16 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -70,33 +70,51 @@ console.log('funkcja zostala wykonana');
 generateTitleLinks();
 function generateTags(){
   /* find all articles */
+
   const articles = document.querySelectorAll(optArticleSelector);
+
   /* START LOOP: for every article: */
+
   for(let article of articles){
     console.log('znaleziono wszystkie artykuly');
+
     /* find tags wrapper */
+
     const tagsWrapper = article.querySelector(optArticleTagsSelector);
      
     /* make html variable with empty string */
+
     let html = ''
+
     /* get tags from data-tags attribute */
+
     const articleTags = article.getAttribute ('data-tags');
+
     /* split tags into array */
+
     console.log (articleTags)
-    const articleTagsarray = articleTags.split(' ');
-    console.log (articleTagsarray);
+    const articleTagsArray = articleTags.split(' ');
+    console.log (articleTagsArray);
+
     /* START LOOP: for each tag */
+
     for(let tag of articleTagsArray){
       /* generate HTML of the link */
       const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
-      /* add generated code to html variable */
+      console.log ('linki zostały wygenerowane!');
 
+      /* add generated code to html variable */
+      html = linkHTML + html;
     /* END LOOP: for each tag */
 
+    }
+
     /* insert HTML of all the links into the tags wrapper */
-  }
+    tagsWrapper.innerHTML = html;
+
+  
   /* END LOOP: for every article: */
-}
+  }
 }
 
 generateTags();
